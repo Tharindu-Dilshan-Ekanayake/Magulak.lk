dda81a57863dcce5b53ba482cb04086ba03d0f46
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-09-26 23:25:35 +0530

    start

--- server/server.js
+++ server/server.js
@@ -1,4 +1,28 @@
-console.log('fuckl')
-y = 3
-x = 5
-console.log(x+y)
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+// Initialize the app before using it
+const app = express();
+
+console.log("hi mfkr", process.env.REACT_A);
+
+mongoose
+  .connect(process.env.REACT_APP_MONGO_URL)
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log('Database not connected', err));
+
+// Middleware
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+
+const port = 8000;
+// Starting the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
+
