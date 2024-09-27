5fb9fb9cc8dddae2541eeddff3c3ae000863468a
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-09-28 00:05:12 +0530

    ss

---
+++ client/src/components/homeCompo/HomeNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomeNav() {
+  return (
+    <div className='bg-[#19191A] flex justify-center items-center'>
+        <div className='border'>
+            <h1 className='text-white text-3xl w-[1440px]'>hiii</h1>
+        </div>
+      
+    </div>
+  )
+}
