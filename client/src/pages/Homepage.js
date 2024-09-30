95d95b1548400fee949e157e8f6d7a10a0ffcb4e
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-09-30 20:55:56 +0530

    sl

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import HomeNav from '../components/homeCompo/HomeNav'
+import Logo from '../components/homeCompo/Logo'
 
 export default function Homepage() {
   return (
     <div>
       <div>
         <HomeNav/>
+      </div>
+      <div className=''> 
+        <Logo/>
       </div>
     </div>
   )
